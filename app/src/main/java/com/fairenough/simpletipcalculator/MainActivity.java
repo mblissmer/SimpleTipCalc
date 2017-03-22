5ccf8ea73ddfaed8df5dd95ab1c49649d57d6cd3
Matt Blissmer <mattblissmer@gmail.com>
2017-03-22 16:16:54 -0400

    -everything looks to be done except rounding, which I still have to do the math on. Probably involves some modulo math. -hid the rounding button for now -going to upload this one. 1.1

--- app/src/main/java/com/fairenough/simpletipcalculator/MainActivity.java
+++ app/src/main/java/com/fairenough/simpletipcalculator/MainActivity.java
@@ -3,10 +3,8 @@ package com.fairenough.simpletipcalculator;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.MenuItemHoverListener;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,111 +12,96 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.NumberPicker;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private double tip = 0.15;
+    private double tip = 0.18;
     private double bill = 0;
-    private int headCount = 0;
-    private DecimalFormat df = new DecimalFormat("0.00");
-    private int roundOption;
-    private TextView tipEachTV;
-    private TextView tipTotalTV;
+    private int headCount = 1;
+    private DecimalFormat df;
+    private boolean doRounding = false;
     private TextView billEachTV;
+    private TextView tipTotalTV;
     private TextView billTotalTV;
+    private TextView extraPenniesTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //set up decimal formatter
+        df = new DecimalFormat("0.00");
+        df.setRoundingMode(RoundingMode.DOWN);
+
         //setting up all fields
-        tipEachTV = (TextView)findViewById(R.id.tipEachNum);
-        tipTotalTV  = (TextView)findViewById(R.id.tipTotalNum);
         billEachTV  = (TextView)findViewById(R.id.billEachNum);
+        tipTotalTV  = (TextView)findViewById(R.id.tipTotalNum);
         billTotalTV = (TextView)findViewById(R.id.billTotalNum);
+        extraPenniesTV = (TextView) findViewById(R.id.extraPennies);
+        final TextView billEachTitle = (TextView)findViewById(R.id.billEachText);
 
         //setting up toolbar
         Toolbar mainToolbar = (Toolbar) findViewById(R.id.mainToolbar);
         setSupportActionBar(mainToolbar);
         //end toolbar
 
-
-        //setting up rounding spinner
-        Spinner roundingOption = (Spinner) findViewById(R.id.roundingOptionSpinner);
-        ArrayAdapter<CharSequence> roundingAdapter = ArrayAdapter.createFromResource(this,R.array.roundFor, android.R.layout.simple_spinner_item);
-        roundingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        roundingOption.setAdapter(roundingAdapter);
-
-        roundingOption.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //rounding switch
+        final Switch rounding = (Switch) findViewById(R.id.roundSwitch);
+        rounding.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                roundOption = position;
-                UpdateTotals();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                doRounding = isChecked;
             }
+        });
 
+        //headcount number picker
+        final NumberPicker hcp = (NumberPicker) findViewById(R.id.headCountPicker);
+        hcp.setMinValue(1);
+        hcp.setMaxValue(10);
+        hcp.setWrapSelectorWheel(false);
+        hcp.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                roundOption = 0;
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                headCount = newVal;
+                if (headCount == 1 && billEachTV.getVisibility() == View.VISIBLE){
+                    billEachTV.setVisibility(View.INVISIBLE);
+                    billEachTitle.setVisibility(View.INVISIBLE);
+                }
+                else if (headCount > 1 && billEachTV.getVisibility() == View.INVISIBLE){
+                    billEachTV.setVisibility(View.VISIBLE);
+                    billEachTitle.setVisibility(View.VISIBLE);
+                }
                 UpdateTotals();
             }
         });
-        //end rounding spinner
 
-        //setting up the headcount spinner
-        Spinner pplCount = (Spinner) findViewById(R.id.headCountSpinner);
-        ArrayAdapter<CharSequence> pplAdapter = ArrayAdapter.createFromResource(this,R.array.headCountArray, android.R.layout.simple_spinner_item);
-        pplAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        pplCount.setAdapter(pplAdapter);
-
-        pplCount.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //tip amount number picker
+        final String[] displayedTipAmounts = {"18", "20", "25", "30"};
+        final NumberPicker tpp = (NumberPicker) findViewById(R.id.tipPercentPicker);
+        tpp.setMinValue(0);
+        tpp.setMaxValue(displayedTipAmounts.length-1);
+        tpp.setDisplayedValues(displayedTipAmounts);
+        tpp.setWrapSelectorWheel(false);
+        tpp.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                headCount = Integer.parseInt(parent.getItemAtPosition(position).toString());
-                //parent.getItemAtPosition(position), returns text of item at that position
-                UpdateTotals();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                headCount = 0;
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                tip = Double.parseDouble(displayedTipAmounts[newVal]) / 100;
                 UpdateTotals();
             }
         });
-        //end headcount spinner
 
-        //setting up the tip amount spinner
-        Spinner tipAmt = (Spinner) findViewById(R.id.tipPercentSpinner);
-        ArrayAdapter<CharSequence> tipAmtAdapter = ArrayAdapter.createFromResource(this,R.array.tipAmountArray, android.R.layout.simple_spinner_item);
-        tipAmtAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        tipAmt.setAdapter(tipAmtAdapter);
 
-        tipAmt.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                tip = Double.parseDouble(parent.getItemAtPosition(position).toString().substring(0,2)) / 100;
-                //parent.getItemAtPosition(position), returns text of item at that position
-                UpdateTotals();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                tip = 0;
-                UpdateTotals();
-            }
-        });
-        //end tip amount spinner
 
 
         //setting up bill text field
@@ -196,64 +179,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void UpdateTotals(){
-
-        double tipEach;
-        double tipTotal;
         double billEach;
+        double tipTotal;
         double billTotal;
-        try {
-        switch (roundOption){
-            case 0: //no rounding
-                tipEach = (bill * tip) / headCount;
-                tipTotal = tipEach * headCount;
-                billTotal = bill + tipTotal;
-                billEach = billTotal / headCount;
-                break;
-            case 1: //round for tip each
-                tipEach = (bill * tip) / headCount;
-                tipEach = roundUp(tipEach);
-                tipTotal = tipEach * headCount;
-                billTotal = bill + tipTotal;
-                billEach = billTotal / headCount;
-                break;
-            case 2: //round for tip total
-                tipTotal = bill * tip;
-                tipTotal = roundUp(tipTotal);
-                tipEach = tipTotal / headCount;
-                billTotal = bill + tipTotal;
-                billEach = billTotal / headCount;
-                break;
-            case 3: //round for bill each
-                tipTotal = bill * tip;
-                billTotal = bill + tipTotal;
-                billEach = billTotal / headCount;
-                billEach = roundUp(billEach);
-                tipEach = billEach - (bill / headCount);
-                tipTotal = tipEach * headCount;
-                billTotal = bill + tipTotal;
-                break;
-            case 4: //round for bill total
-                tipTotal = (bill * tip);
-                billTotal = bill + tipTotal;
-                billTotal = roundUp(billTotal);
-                tipTotal = billTotal - bill;
-                tipEach = tipTotal / headCount;
-                billEach = billTotal / headCount;
-                break;
-            default:
-                throw new ArithmeticException();
-        }
-        }catch (ArithmeticException ex){
-            tipEach = 0;
-            tipTotal = 0;
-            billEach = 0;
-            billTotal = 0;
-        }
+        if (doRounding){
+            //TODO
+            tipTotal = tip * bill;
+            billTotal = bill + tipTotal;
+            billEach = billTotal / headCount;
+            //replace this, just wanted to suppress a warning
+        } else {
+            tipTotal = tip * bill;
+            billTotal = bill + tipTotal;
+            billEach = billTotal / headCount;
 
-        tipEachTV.setText(df.format(tipEach));
-        tipTotalTV.setText(df.format(tipTotal));
+
+        }
+        double penniesEach = Math.floor(billEach * 100);
+        double penniesTotal = Math.floor(billTotal * 100);
+        int pennies = findExtraPennies((int)penniesEach, (int)penniesTotal);
+        String penniesString = "";
+        if (pennies > 0) {
+            if (pennies == 1){
+                penniesString = getResources().getText(R.string.penniesPt1) + String.valueOf(pennies) + " " + getResources().getText(R.string.pennyPt2);
+            }
+            else {
+                penniesString = getResources().getText(R.string.penniesPt1) + String.valueOf(pennies) + " " + getResources().getText(R.string.penniesPt2);
+            }
+        }
+        extraPenniesTV.setText(penniesString);
         billEachTV.setText(df.format(billEach));
+        tipTotalTV.setText(df.format(tipTotal));
         billTotalTV.setText(df.format(billTotal));
 
+    }
+
+    int findExtraPennies(int billEach, int billTotal){
+        return billTotal - (billEach * headCount);
     }
 }
